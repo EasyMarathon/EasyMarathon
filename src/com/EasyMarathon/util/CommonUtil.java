afeb3b31a6d0e6f734bff5416fa9abb6b7aee214
gecongcong <congcong.ge@foxmail.com>
2016-04-07 22:46:24 +0800

    3

--- src/com/EasyMarathon/util/CommonUtil.java
+++ src/com/EasyMarathon/util/CommonUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.ConnectException;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
@@ -116,5 +117,42 @@ public class CommonUtil {
 			}
 		}
 		return token;
+	}
+	
+	/**
+	 * URL编码（utf-8）
+	 * 
+	 * @param source
+	 * @return
+	 */
+	public static String urlEncodeUTF8(String source) {
+		String result = source;
+		try {
+			result = java.net.URLEncoder.encode(source, "utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	/**
+	 * 根据内容类型判断文件扩展名
+	 * 
+	 * @param contentType 内容类型
+	 * @return
+	 */
+	public static String getFileExt(String contentType) {
+		String fileExt = "";
+		if ("image/jpeg".equals(contentType))
+			fileExt = ".jpg";
+		else if ("audio/mpeg".equals(contentType))
+			fileExt = ".mp3";
+		else if ("audio/amr".equals(contentType))
+			fileExt = ".amr";
+		else if ("video/mp4".equals(contentType))
+			fileExt = ".mp4";
+		else if ("video/mpeg4".equals(contentType))
+			fileExt = ".mp4";
+		return fileExt;
 	}
 }
