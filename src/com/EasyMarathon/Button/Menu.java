bc97dcf2fba86898058e9ce8723510cfaaa72d4d
gecongcong <congcong.ge@foxmail.com>
2016-04-07 11:23:12 +0800

    gcc commit

---
+++ src/com/EasyMarathon/Button/Menu.java
@@ -0,0 +1,19 @@
+package com.EasyMarathon.Button;
+
+/**
+ * ²Ëµ¥
+ * 
+ * @author liufeng
+ * @date 2013-10-15
+ */
+public class Menu {
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+}
