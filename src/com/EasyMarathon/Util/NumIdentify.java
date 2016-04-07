fcfcffbdb7112bcb76723cd4fede432a1c5f20dd
Chen Zack <czktc2007@gmail.com>
2016-04-07 18:36:26 +0800

    fix bug in PicDAO,add numtest(wothout c++ lib)

--- src/com/EasyMarathon/Util/NumIdentify.java
+++ src/com/EasyMarathon/Util/NumIdentify.java
@@ -1,11 +1,25 @@
 package com.EasyMarathon.Util;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class NumIdentify
 {
 	public native int Identify(String fname);
 
 	static
 	{
-		System.loadLibrary("NumIdentify");
+		String url = NumIdentify.class.getResource("").getPath();//.substring(1);
+		String uri = "";
+		try
+		{
+			uri = new URI(url).getPath();
+		}
+		catch (URISyntaxException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.load(uri + System.mapLibraryName("NumIdentify"));
 	}
 }
---
+++ src/com/EasyMarathon/test/NumTest.java
@@ -0,0 +1,15 @@
+package com.EasyMarathon.test;
+
+import com.EasyMarathon.Util.NumIdentify;
+
+public class NumTest
+{
+	public static void main(String[] args)
+	{
+		String fname = "D:\\Programs Data\\VSProject\\electronicBusiness\\electronicBusiness\\IMG_9998OK.JPG";
+		NumIdentify ni = new NumIdentify();
+		System.out.println("run dll");
+		//int ans = ni.Identify(fname);
+		//System.out.println("ans:"+ans);
+	}
+}
