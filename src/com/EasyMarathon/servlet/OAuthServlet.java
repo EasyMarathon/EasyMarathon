59f8817cbc46468809392266021fb08b04090678
gecongcong <congcong.ge@foxmail.com>
2016-04-08 18:30:10 +0800

    2222

--- src/com/EasyMarathon/servlet/OAuthServlet.java
+++ src/com/EasyMarathon/servlet/OAuthServlet.java
@@ -20,16 +20,19 @@ public class OAuthServlet extends HttpServlet {
 	private static final long serialVersionUID = -1847238807216447030L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("gb2312");
-		response.setCharacterEncoding("gb2312");
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 
 		// 用户同意授权后，能获取到code
 		String code = request.getParameter("code");
 
 		// 用户同意授权
 		if (!"authdeny".equals(code)) {
+			System.out.println("code:"+code);
 			// 获取网页授权access_token
 			WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken("APPID", "APPSECRET", code);
+			System.out.println(weixinOauth2Token);
+			
 			// 网页授权接口访问凭证
 			String accessToken = weixinOauth2Token.getAccessToken();
 			// 用户标识
