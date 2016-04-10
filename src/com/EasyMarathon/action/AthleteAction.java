d7dcece062a5e2cc7d1f3d616b0bbfff86f6c90e
Chen Zack <czktc2007@gmail.com>
2016-04-11 00:19:36 +0800

    change action for Pic,little code clean

--- src/com/EasyMarathon/action/AthleteAction.java
+++ src/com/EasyMarathon/action/AthleteAction.java
@@ -1,57 +1,70 @@
 package com.EasyMarathon.action;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
+import com.EasyMarathon.bean.PicBean;
 import com.EasyMarathon.bean.SNSUserInfo;
-import com.EasyMarathon.bean.UserBean;
-import com.EasyMarathon.dao.PictureDao.Status;
 import com.EasyMarathon.service.AthleteService;
 import com.opensymphony.xwork2.ActionContext;
 
-public class AthleteAction {
+public class AthleteAction
+{
 	SNSUserInfo snsUserInfo;
 	int eventID;
 	int aID;
-	public SNSUserInfo getSnsUserInfo() {
+
+	public SNSUserInfo getSnsUserInfo()
+	{
 		return snsUserInfo;
 	}
-	public void setSnsUserInfo(SNSUserInfo snsUserInfo) {
+
+	public void setSnsUserInfo(SNSUserInfo snsUserInfo)
+	{
 		this.snsUserInfo = snsUserInfo;
 	}
-	public int getaID() {
+
+	public int getaID()
+	{
 		return aID;
 	}
-	public void setaID(int aID) {
+
+	public void setaID(int aID)
+	{
 		this.aID = aID;
 	}
-	public int getEventID() {
+
+	public int getEventID()
+	{
 		return eventID;
 	}
-	public void setEventID(int eventID) {
+
+	public void setEventID(int eventID)
+	{
 		this.eventID = eventID;
 	}
 
 	public String lockInfo()
 	{
-	
-		//首先判断运动员是否报名，没有报名提示
-		//报名的
-		if(AthleteService.Bind(snsUserInfo.getOpenId(), eventID, aID))
+
+		// 首先判断运动员是否报名，没有报名提示
+		// 报名的
+		if (AthleteService.Bind(snsUserInfo.getOpenId(), eventID, aID))
 		{
 			return "success";
 		}
-		
+
 		else
 			return "false";
 	}
+
 	public String findPic()
 	{
 		ActionContext actionContext = ActionContext.getContext();
-		Map<String,Object> session = actionContext.getSession();
-		HashMap<String, Status> picutres=new HashMap<String, Status>();
-		picutres=AthleteService.returnPicture(aID, eventID);
-		if(picutres!=null)
+		Map<String, Object> session = actionContext.getSession();
+		ArrayList<PicBean> picutres = new ArrayList<>();
+		picutres = AthleteService.returnPicture(aID, eventID);
+		if (picutres != null)
 		{
 			session.put("pictures", picutres);
 			return "success";
--- src/com/EasyMarathon/action/PicAction.java
+++ src/com/EasyMarathon/action/PicAction.java
@@ -1,51 +1,53 @@
 package com.EasyMarathon.action;
 
-import java.io.*;
-
-import com.EasyMarathon.bean.MsgBean;
 import com.EasyMarathon.bean.PicBean;
 import com.EasyMarathon.service.PicService;
-import com.opensymphony.xwork2.ActionSupport;
 
-
-public class PicAction 
+public class PicAction
 {
-	private static final long serialVersionUID = 1L;
 	private PicBean picture;
 	private String eventID;
 
-	public PicBean getPicture() {
+	public PicBean getPicture()
+	{
 		return picture;
 	}
-	public void setPicture(PicBean picture) {
+
+	public void setPicture(PicBean picture)
+	{
 		this.picture = picture;
 	}
-	public String getEventID() {
+
+	public String getEventID()
+	{
 		return eventID;
 	}
-	public void setEventID(String eventID) {
+
+	public void setEventID(String eventID)
+	{
 		this.eventID = eventID;
 	}
-	public String uploadPic(){
-		//System.out.println(picture.getFile().getAbsolutePath());
+
+	public String uploadPic()
+	{
+		// System.out.println(picture.getFile().getAbsolutePath());
 		PicService picservice = new PicService();
-		
-		if(picservice.uploadPicService(picture.getFile(),eventID))
-		    return "success";
-		else
-			return "fail";
-	}
-	public String uploadPicforUser(){
-		//System.out.println(picture.getFile().getAbsolutePath());
-		PicService picservice = new PicService();
-		
-		if(picservice.uploadPicforUserService(picture.getFile(),eventID))
-		    return "success";
+
+		if (picservice.uploadPicService(picture.getFile(), eventID))
+			return "success";
 		else
 			return "fail";
 	}
 
-	
+	public String uploadPicforUser()
+	{
+		// System.out.println(picture.getFile().getAbsolutePath());
+		PicService picservice = new PicService();
 
-	
+		if (picservice.uploadPicforUserService(picture.getFile(), eventID))
+			return "success";
+		else
+			return "fail";
+	}
+
 }
